756633caef3e12b316442cd00afc6b34195ea713
minal-bsm <minal.bsm@gmail.com>
2024-12-01 21:23:08 +0530

    unit tests added

--- mycv/src/users/auth.service.spec.ts
+++ mycv/src/users/auth.service.spec.ts
@@ -2,15 +2,25 @@ import { Test } from "@nestjs/testing";
 import { AuthService } from "./auth.service";
 import { UsersService } from "./users.service";
 import { User } from "./user.entity";
+import { BadRequestException, NotFoundException } from "@nestjs/common";
 
 describe('AuthService', () => {
-    let service: AuthService;;
+    let service: AuthService;
+    let fakeUsersService: Partial<UsersService>;
 
     beforeEach(async () => {
         //Create a fake copy of the users service
-        const fakeUsersService: Partial<UsersService> = {
-            find: () => Promise.resolve([]),
-            create: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
+        const users: User[] = [];
+        fakeUsersService = {
+            find: (email: string) => {
+                const filteredUsers = users.filter(user => user.email === email); //filter the users array to find the user with the given email
+                return Promise.resolve(filteredUsers);
+            },
+            create: (email: string, password: string) => {
+                const user = {id: Math.floor( Math.random() * 999999), email, password} as User; //create a new user with a random id
+                users.push(user); //add the user to the users array
+                return Promise.resolve(user); //return the user
+            }
         };
     
         const module = await Test.createTestingModule({
@@ -24,11 +34,41 @@ describe('AuthService', () => {
         }).compile();
     
         service = module.get(AuthService);
-    })
+    });
     
     it('can create an instance of auth service', async () => {
-        
-    
         expect(service).toBeDefined();
-    })
+    });
+
+    //SIGN UP TESTS
+    it('creates a new user with a salted and hashed password', async() => {
+        const user = await service.signup('asdf@asdf.com', 'asdf'); 
+        //making sure password was salted and hashed in some way
+        expect(user.password).not.toEqual('asdf');
+        const [salt,hash] = user.password.split('.');
+        expect(salt).toBeDefined();
+        expect(hash).toBeDefined();
+    });
+
+    it('throws an error if user signs up with email that is in use', async() => {
+        await service.signup('asdf@asdf.com', 'asdf'); //sign up with email
+        await expect(service.signup('asdf@asdf.com', 'asdf')).rejects.toThrow(BadRequestException); //throw a BadRequestException if signing up with an email that is already in use
+    });
+
+    //SIGN IN TESTS
+
+    it('throws an error if signin is called with an unused email', async() => {
+        await expect( service.signin('asdflk@asdflk.com', 'asdflk')).rejects.toThrow(NotFoundException); //throw an NotFoundException if signing in with unused email
+    });
+
+    it('throws an error if an invalid password is provided', async() => {
+        await service.signup('laskdjf@alskdjf.com', 'password'); //sign up with email and password
+        await expect(service.signin('laskdjf@alskdjf.com', 'wrongpassword')).rejects.toThrow(BadRequestException); //throw a BadRequestException if signing in with wrong password
+    });
+
+    it('returns a user if correct password is provided', async() => {
+        await service.signup('asdf@asdf.com', 'mypassword');
+        const user = await service.signin('asdf@asdf.com', 'mypassword'); //sign in with correct password
+        expect(user).toBeDefined(); //make sure user is defined
+    }); 
 });
--- mycv/src/users/users.controller.spec.ts
+++ mycv/src/users/users.controller.spec.ts
@@ -1,12 +1,40 @@
 import { Test, TestingModule } from '@nestjs/testing';
 import { UsersController } from './users.controller';
+import { UsersService } from './users.service';
+import { AuthService } from './auth.service';
+import { User } from './user.entity';
+import { NotFoundException } from '@nestjs/common';
 
 describe('UsersController', () => {
   let controller: UsersController;
+  let fakeUsersService: Partial<UsersService>;
+  let fakeAuthService: Partial<AuthService>;
 
   beforeEach(async () => {
+    fakeUsersService = {
+      find: (email: string) => {
+        return Promise.resolve([{id: 1, email, password: 'asdf'}] as User[]);
+      },
+      findOne: (id:number) => {
+        return Promise.resolve({id, email: 'asdf@asdf.com', password: 'asdf'} as User)
+      },
+    };
+    fakeAuthService = {
+      signin: (email: string, password: string) => Promise.resolve({id: 1, email, password} as User)
+    };
+
     const module: TestingModule = await Test.createTestingModule({
       controllers: [UsersController],
+      providers: [
+        {
+          provide: UsersService,
+          useValue: fakeUsersService
+        },
+        {
+          provide: AuthService,
+          useValue: fakeAuthService
+        }
+      ]
     }).compile();
 
     controller = module.get<UsersController>(UsersController);
@@ -14,5 +42,28 @@ describe('UsersController', () => {
 
   it('should be defined', () => {
     expect(controller).toBeDefined();
+  });
+
+  it('findAllUsers returns a list of users with the given email', async () => {
+    const users = await controller.findAllUsers('asdf@asdf.com'); // will return the users array from fakeUsersService.find with id 1 and password asdf
+    expect(users.length).toEqual(1);
+    expect(users[0].email).toEqual('asdf@asdf.com');
+  });
+
+  it('findUser returns a single user with the given id', async () => {
+    const user = await controller.findUser('1'); // will return the user with id 1 from fakeUsersService.findOne
+    expect(user).toBeDefined();
+  });
+
+  it('findUser throws an error if user with given id is not found', async () => {
+    fakeUsersService.findOne = () => null;
+    await expect(controller.findUser('1')).rejects.toThrow(NotFoundException);
+  });
+
+  it('signin updates session object and returns user', async () => {
+    const session = { userId: -10 }; // signin() will return the user with id 1 from fakeAuthService.signin
+    const user = await controller.signin({email: 'asdf@asdf.com', password: 'asdf'}, session); // will return the user with id 1 from fakeAuthService.signin
+    expect(user.id).toEqual(1);
+    expect(session.userId).toEqual(1);
   });
 });
